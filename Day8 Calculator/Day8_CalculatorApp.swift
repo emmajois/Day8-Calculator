a61a32e95566333160e7a52638f2763a9bf7028a
Steve Liddle <liddle@byu.edu>
2023-09-27 21:30:55 -0600

    Initial Commit

---
+++ Day8 Calculator/Day8_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day8_CalculatorApp.swift
+//  Day8 Calculator
+//
+//  Created by Stephen Liddle on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Day8_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
